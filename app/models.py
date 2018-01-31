65385aad2a680c70dd4391f618bb82b5c141e927
market <market@market.local>
2018-01-31 14:44:42 -0600

    Add abstract user model.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+class User(AbstractUser):
+    pass
