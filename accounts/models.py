680060dd6bd46d00cbae65b6f772d9debb31e7dc
Alfredo Yebra Jr <fred.yebra@gmail.com>
2018-02-19 15:38:08 -0600

    Add accounts model test for address.

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,11 @@ from django.contrib.auth.models import AbstractUser
 
 class Profile(AbstractUser):
     phone_number = models.CharField(max_length=20, blank=True)
+    street = models.CharField(max_length=255, blank=True)
     city = models.CharField(max_length=255, blank=True)
     state = models.CharField(max_length=255, blank=True)
     zipcode = models.CharField(max_length=255, blank=True)
+
+    @property
+    def address(self):
+        return ', '.join([self.street.title(), self.city, self.state.upper(), self.zipcode])
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,22 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Profile
+
+
+class ProfileTestCase(TestCase):
+
+    def setUp(self):
+        Profile.objects.create(
+            email='test1@hotmail.com',
+            first_name='First1',
+            last_name='Last1',
+            street='100 West Ave',
+            city='Austin',
+            state='tx',
+            zipcode='78039')
+
+    def test_profile_address(self):
+        profile = Profile.objects.get(email='test1@hotmail.com')
+        self.assertEqual(profile.address, '100 West Ave, Austin, TX, 78039')
