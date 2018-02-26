c2f9d7598718da524ccc903fc7f3c00e2f37f671
Alfredo Yebra Jr <fred.yebra@gmail.com>
2018-02-25 19:36:20 -0600

    Add form styles.

--- resumes/models.py
+++ resumes/models.py
@@ -5,12 +5,13 @@ from django.db import models
 class Resume(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    @property
+    def sections(self):
+        return ResumeSection.objects.filter(resume=self)
+
 
 class ResumeSection(models.Model):
     resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
-
-    class Meta:
-        abstract = True
 
 
 class Experience(ResumeSection):
@@ -18,15 +19,27 @@ class Experience(ResumeSection):
     start_month_year = models.CharField(max_length=4, blank=False)
     end_month_year = models.CharField(max_length=4, blank=False)
 
+    def __str__(self):
+        return '{} - {} {}'.format(self.title, self.start_month_year, self.end_month_year)
+
 
 class Education(ResumeSection):
     school = models.CharField(max_length=255, blank=False)
+
+    def __str__(self):
+        return '{}'.format(self.school)
 
 
 class Skills(ResumeSection):
     skill = models.CharField(max_length=100, blank=True)
 
+    def __str__(self):
+        return self.skill
+
 
 class Awards(ResumeSection):
     award = models.CharField(max_length=255, blank=True)
-    year = models.IntegerField(blank=False)
+    year = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.award, str(self.year))
